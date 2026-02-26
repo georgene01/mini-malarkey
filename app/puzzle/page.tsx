8c2e05be2051011f31145e6ee45be151ce480221
georgene01 <dwtgeo002@myuct.ac.za>
2026-02-26 06:47:27 +0200

    Make login page the root entry point

--- app/puzzle/page.tsx
+++ app/puzzle/page.tsx
@@ -117,10 +117,7 @@ export default function HomePage() {
     checkProfile()
   }, [user])
 
-  useEffect(() => {
-    setShowChickenSplash(true)
-  }, [])
-  
+
   useEffect(() => {
     if (!user || !puzzle) return
   
